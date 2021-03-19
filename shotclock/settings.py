849d29d67b571484aa04a71e7501c5b99c59ffd7
Ethan Kuoch <65685915+ethan-25@users.noreply.github.com>
2021-03-19 17:33:54 -0400

    Update settings.py

--- shotclock/settings.py
+++ shotclock/settings.py
@@ -124,5 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-SITE_ROOT = 'info\static'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'info', 'static')]
